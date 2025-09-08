508f4325df926baea466e2effb7289298d23a094
Eduardo Battisti <eduardobattisti10@gmail.com>
2025-09-08 14:18:11 -0300

    refactor: undo changes

--- dot_config/nvim/lua/plugins/lazygit.lua
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -1,16 +1,10 @@
 return {
   'kdheepak/lazygit.nvim',
-  cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
-  },
+  -- optional for floating window border decoration
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
   keys = {
-    { '<Leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
 }
--- dot_config/nvim/lua/plugins/vim-doge.lua
+++ dot_config/nvim/lua/plugins/vim-doge.lua
@@ -1,3 +1,20 @@
 return {
   'kkoomen/vim-doge',
+  event = 'BufRead',
+  cmd = { 'DogeGenerate', 'DogeCreateDocStandard' },
+  config = function()
+    vim.g.doge_enable_mappings = 0
+    vim.g.doge_doc_standard_python = 'sphinx'
+    vim.g.doge_doc_standard_javascript = 'jsdoc'
+    vim.g.doge_doc_standard_typescript = 'jsdoc'
+    vim.g.doge_doc_standard_php = 'phpdoc'
+    vim.g.doge_doc_standard_lua = 'ldoc'
+    vim.g.doge_doc_standard_java = 'javadoc'
+    vim.g.doge_doc_standard_c = 'kernel_doc'
+    vim.g.doge_doc_standard_cpp = 'cpp_doxygen'
+    vim.g.doge_doc_standard_sh = 'google'
+    
+    -- Custom mapping
+    vim.keymap.set('n', '<leader>dg', '<Plug>(doge-generate)', { desc = 'Generate documentation' })
+  end,
 }
